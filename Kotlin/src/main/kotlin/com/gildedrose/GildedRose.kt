f247e3647098f287c6aec560431efaa8777be279
xueminguan <gxuemin@gmail.com>
2022-08-03 16:47:37 +0100

    JB + XG: extract increment and decrement for quality.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,52 +2,62 @@ package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
 
+    private fun decrementQuality(item:Item, byAmount:Int = 1){
+        item.quality = item.quality - byAmount
+    }
+
+    private fun incrementQuality(item:Item){
+        item.quality = item.quality + 1
+    }
+
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+        for (item in items) {
+            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.quality > 0) {
+                    when {
+                        item.name != "Sulfuras, Hand of Ragnaros" -> {
+                            decrementQuality(item)
+                        }
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                if (item.quality < 50) {
+                    incrementQuality(item)
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                incrementQuality(item)
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                incrementQuality(item)
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.sellIn = item.sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.name != "Aged Brie") {
+                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.quality > 0) {
+                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                                decrementQuality(item)
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        decrementQuality(item, item.quality)
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (item.quality < 50) {
+                        incrementQuality(item)
                     }
                 }
             }
