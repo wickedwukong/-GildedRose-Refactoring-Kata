56e4e4226d0516c169faad9db0e84b69d9aac85c
xueminguan <gxuemin@gmail.com>
2022-08-03 17:28:23 +0100

    JB + XG: fixed the test by giving the correct name for Siulfuras.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,7 +3,7 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
     fun updateQuality() {
         for (item in items) {
-            item.update()
+            item.updater(item)
         }
     }
 
